6a46266a1814a915f5eb608b5fd08026f1133e02
aparna <aparnaarumugam@gmail.com>
2019-07-02 13:27:58 +0530

    assignment

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -14,32 +14,6 @@ router.post('/images',(req, res) => {
      newUser.save()
      .then(() => res.json('User added!'))
      .catch(err => res.status(400).json(err));
-  // if (typeof img == 'string') {
-     
-  // } else {
-  //   const promise_array = []
-  //   img.forEach(i => {
-  //     console.log('multiple' , i);
-  //     const newUser= new User({image: i, description});
-  //     promise_array.push(
-  //       new Promise(
-  //         (resolve, reject) => {
-  //           newUser.save()
-  //           .then(() => resolve('User added!'))
-  //           .catch(err => reject(err));
-  //         }
-  //       )
-  //     )
-  //   });
-  //   Promise.all(promise_array).then(
-  //     (responses) => {
-  //       res.json(responses);
-  //     }
-  //   );
-  // }
-
-  
-
 });
 
 module.exports = router;
