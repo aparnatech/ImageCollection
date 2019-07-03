03bd3a1e56d00a880cbe2beb3a53e628e434c725
aparnatech <41115739+aparnatech@users.noreply.github.com>
2019-07-03 16:58:23 +0530

    Update upload.jsx
    added new button to clear the uploaded image

--- src/components/upload.jsx
+++ src/components/upload.jsx
@@ -9,22 +9,26 @@ class upload extends Component {
         super(props);
         this.fileSelectedHandler =  this.fileSelectedHandler.bind(this);
         this.onChangedescription =  this.onChangedescription.bind(this);
+        this.cleartheImage = this.cleartheImage.bind(this);
         this.onSubmit =  this.onSubmit.bind(this);
         this.state = {
             image: '',
             description: ''
         }
     }
+    
     fileSelectedHandler (files) {
         this.setState({
             image: files.base64
         })
     } 
+    
     onChangedescription(e) {
         this.setState({
             description: e.target.value
         })
     }
+    
     onSubmit(e) {
         e.preventDefault();
         if(this.state.image === ''){
@@ -45,8 +49,17 @@ class upload extends Component {
                 })
             });
         }
-        
     }
+    
+     cleartheImage() {
+        if(this.state.image === '') {
+            alert('No image to Unselect');
+        } else {
+            this.setState({image: ''});
+            alert('successfully Unselected the image')
+        }
+    }
+    
     render() {
         return (
           <div className="center">
@@ -54,13 +67,14 @@ class upload extends Component {
             <ReactFileReader handleFiles={this.fileSelectedHandler} base64={true} multipleFiles={true}>
             <button type="button" className='btn'>choose Image</button>
             </ReactFileReader>
+            <button className="btn unselect_style" type="button" onClick={this.cleartheImage}>unselect Image</button>
             <br/>
             <input  type="text" placeholder="about the image..." value={this.state.description} className="uploading_input" onChange={this.onChangedescription} required />
-            <div><button className="btn" type="submit">upload</button></div>
-            
+            <div><button className="waves-effect waves-light btn-small" type="submit"><i className="material-icons left">cloud</i>upload</button></div>  
             </form>   
           </div>
         );
     }
 }
+
 export default upload;
