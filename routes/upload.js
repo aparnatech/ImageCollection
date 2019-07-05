961042bc2788b37623e9ade7734e6d2afaf8b42a
aparna <aparnaarumugam@gmail.com>
2019-07-05 23:19:11 +0530

    deployment

--- routes/upload.js
+++ routes/upload.js
@@ -6,7 +6,7 @@ router.get('/',(req, res) => {
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
-router.post('/images',(req, res) => {
+router.post('images',(req, res) => {
   const img = req.body.image;
   const description = req.body.description;
   const date = req.body.date;
