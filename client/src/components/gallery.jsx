1deb19f7acef917072649cf989cdaf70c5684ab5
aparna <aparnaarumugam@gmail.com>
2019-07-09 16:39:26 +0530

    done

--- client/src/components/gallery.jsx
+++ client/src/components/gallery.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './gallery.css';
 const axios = require('axios');
+const moment = require('moment');
+var searched1;
+
 export default class Gallery extends Component {
   constructor() {
     super();
     this.iterateImage = this.iterateImage.bind(this);
     this.onsearchhandleChange = this.onsearchhandleChange.bind(this);
     this.RandomizeFun = this.RandomizeFun.bind(this);
+    this.DateFilter = this.DateFilter.bind(this);
+    this.Description =  this.Descriiption.bind(this);
+    // this.date = this.date.bind(this);
     this.state = {
       datas: [],
       search: '',
@@ -34,6 +40,31 @@ export default class Gallery extends Component {
       })
     )
   }
+  DateFilter() {
+    // 
+    if (moment(this.state.search, "DD/MM/YYYY", true).isValid()) {
+      var initial = this.state.search.split(/\//).reverse().join('-');
+      console.log(initial);
+      searched1 =  this.state.datas.filter(img => {
+          var date1 = new Date(img.date);
+          return date1.toISOString().substring(0, 10) === initial;
+        });
+        console.log('date@@@@@@@@', searched1);
+        // this.setState({
+        //   datas: [...searched1]
+        // });
+        console.log('st*******', this.state.datas);
+  }
+}
+  Descriiption() {
+    if((/^[a-zA-Z]+$/.test(this.state.search))) {
+      console.log('oo',this.state.search);
+       const searched  =  this.state.datas.filter(img => {
+        return img.description.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+      })
+      console.log('searched', searched);
+  }
+}
   onsearchhandleChange(e) {
       this.setState({
         search: e.target.value
@@ -47,16 +78,34 @@ export default class Gallery extends Component {
       datas : [...ShuffleData]
     })    
   }
+  // date(formateDate){
+  //   return new Date(formateDate);
+  // }
   render() {
     if(this.state.datas.length) {
+      var imageFilter;
       const {search} = this.state;
-      const imageFilter = this.state.datas.filter(img => {
-      return img.description.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      console.log(search,'render');
+      // const imageFilter12 = searched ;
+     imageFilter = this.state.datas.filter(img => {
+        if(img.description) {
+          return img.description.toLowerCase().indexOf(search.toLowerCase()) !== -1
+        // }     if(moment(this.state.search, "DD/MM/YYYY", true).isValid()) {
+        //   imageFilter = searched1
+          // this.state.datas.filter(img => {
+          //   var initial = this.state.search.split(/\//).reverse().join('-');
+          //   var date1 = new Date(img.date);
+          //   return date1.toISOString().substring(0, 10) === initial;
+          // });
+        }
+     
     })
       return (
         <div className="container">
-           <input type="text" className="inputdesign"  onChange={this.onsearchhandleChange}  placeholder="Search Image with description..." />
-           <button className="btn button_style" onClick={this.RandomizeFun}>Randomize</button>
+           <input type="text" className="inputdesign"  onBlur={this.onsearchhandleChange}  placeholder="Search Image with description..." />
+           <button className="btn button_style" onClick={this.RandomizeFun}>List images</button>
+           <button className="btn button_style" onClick={this.DateFilter}>Date</button>
+           <button className="btn button_style" onClick={this.Description}>Description</button>
              { imageFilter.map((link) => {
                 return(
                   <div className="whole_div" key={link._id}>
